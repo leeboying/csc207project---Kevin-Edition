61c0c3a2abc742d72c96c366cbd181f8a5ad3c70
William Young <will.young@mail.utoronto.ca>
2023-10-31 01:39:08 -0400

    castling logic and wouldBeCheck helper method

--- src/entity/King.java
+++ src/entity/King.java
@@ -29,55 +29,68 @@ public class King extends Piece {
                 Move potentialMove = makeMove(position, target);
 
                 // check that king is not in check
-                HashMap<ArrayList<Integer>, Piece> tempBoardState = new HashMap<>(boardState);
-
-                tempBoardState.put(target, this);
-                tempBoardState.put(position, null);
-
-                if (!isInCheck(target, tempBoardState)) {
+                if (!wouldBeCheck(position, boardState, target)) {
                     validMoves.add(potentialMove);
                 }
+
             } else if (square.equals("enemy")) {
                 Move potentialMove = makeCapture(position, target);
 
                 // check that king is not in check
-                HashMap<ArrayList<Integer>, Piece> tempBoardState = new HashMap<>(boardState);
-
-                tempBoardState.put(target, this);
-                tempBoardState.put(position, null);
-
-                if (!isInCheck(target, tempBoardState)) {
+                if (!wouldBeCheck(position, boardState, target)) {
                     validMoves.add(potentialMove);
                 }
             }
         }
 
         // castling special case
-        if (!hasMoved) { // king has not moved yet
+        if (!hasMoved && !isInCheck(position, boardState)) { // king has not moved yet and is not in check
             int y = position.get(1);
+
             Piece potentialRightRook = boardState.get(coordinateBuilder(1, y));
             Piece potentialLeftRook = boardState.get(coordinateBuilder(8, y));
+
             if (potentialRightRook instanceof Rook && !potentialRightRook.hasMoved // rook at a1/a8 that has not moved
-                    && boardState.get(coordinateBuilder(2, y)) == null // no pieces in between king and rook
-                    && boardState.get(coordinateBuilder(3, y)) == null) {
+                    && boardState.get(coordinateBuilder(6, y)) == null // no pieces in between king and rook
+                    && boardState.get(coordinateBuilder(7, y)) == null) {
 
-                HashMap<ArrayList<Integer>, Piece> tempBoardState = new HashMap<>(boardState);
+                if (!wouldBeCheck(position, boardState, coordinateBuilder(6, y)) // does not castle through or into check
+                        && !wouldBeCheck(position, boardState, coordinateBuilder(7, y))) {
 
-                tempBoardState.put(coordinateBuilder(3, y), this);
-                tempBoardState.put(position, null);
+                    Move castleRight = new Move(this, position, coordinateBuilder(7, y));
+                    castleRight.setIsCastle();
 
-
+                    validMoves.add(castleRight);
+                }
             }
 
-            if (potentialRightRook instanceof Rook && !potentialRightRook.hasMoved // rook at a1/a8 that has not moved
-                    && ) {
+            if (potentialLeftRook instanceof Rook && !potentialLeftRook.hasMoved // rook at h1/h8 that has not moved
+                    && boardState.get(coordinateBuilder(2, y)) == null // no pieces in between
+                    && boardState.get(coordinateBuilder(3, y)) == null
+                    && boardState.get(coordinateBuilder(4, y)) == null) {
 
+                if (!wouldBeCheck(position, boardState, coordinateBuilder(3, y)) // does not castle through or into check
+                        && !wouldBeCheck(position, boardState, coordinateBuilder(4, y))) {
+
+                    Move castleLeft = new Move(this, position, coordinateBuilder(3, y));
+                    castleLeft.setIsCastle();
+
+                    validMoves.add(castleLeft);
+                }
             }
         }
-
         return validMoves.toArray(new Move[0]);
     }
 
+    private boolean wouldBeCheck(ArrayList<Integer> position, HashMap<ArrayList<Integer>, Piece> boardState,
+                                ArrayList<Integer> newPos) {
+        HashMap<ArrayList<Integer>, Piece> tempBoardState = new HashMap<>(boardState);
+
+        tempBoardState.put(newPos, this);
+        tempBoardState.put(position, null);
+
+        return isInCheck(newPos, tempBoardState);
+    }
 
     public boolean isInCheck(ArrayList<Integer> position, HashMap<ArrayList<Integer>, Piece> boardState) {
         return false;
